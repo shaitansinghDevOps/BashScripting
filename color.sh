b58b0ff9da564a39e369d7e9e567260e3a0f3cfe
satyam singh <satyamsingh@satyams-MacBook-Air.local>
2026-05-06 09:30:59 +0530

    first commit

--- color.sh
+++ color.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo -e "\e[34m test the color code\e[0m"
 echo -e "\e[31 test the Red Color\e[0m"
+echo -e "\e[34m test the color code \e[0m"
+echo -e "\e[31m test the Red Color \e[0m"
