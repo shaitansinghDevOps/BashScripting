fae7ed5b62b038463abbea8410fcdf9bb77f238c
satyam singh <satyamsingh@satyams-MacBook-Air.local>
2026-05-07 16:38:50 +0530

    first commit

--- RoboShopProjectAutomation/frontend.sh
+++ RoboShopProjectAutomation/frontend.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 echo "Configuration management for frontend in progress"
-
+ID=$(id-u)
+if[$id -ne 0]
+do
+ echo "script has to excute as root user"
+ echo -e "Example usegae \n\t \e[32 sudo bash $0 OR #bash $0 \e[0m"
+fi
 echo "disbling the nagnix"
 dnf module disable nginx -y
 
