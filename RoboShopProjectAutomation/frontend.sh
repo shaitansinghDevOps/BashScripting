9b25894b61d9015adf4ba0174d4c2a6463e25be5
satyam singh <satyamsingh@satyams-MacBook-Air.local>
2026-05-07 23:06:22 +0530

    first commit

--- RoboShopProjectAutomation/frontend.sh
+++ RoboShopProjectAutomation/frontend.sh
@@ -3,6 +3,7 @@
 echo "Configuration management for frontend in progress"
 ID=$(id -u)
 Component="frontend"
+Log=$("/tmp/$Component.log")
 if [ $ID -ne 0 ]
 then
  echo "script has to excute as root user"
@@ -10,22 +11,22 @@ then
  exit 1
 fi
 echo "disbling the nagnix"
-dnf module disable nginx -y
+dnf module disable nginx -y &>> $Log
 
 echo "enbling nagix"
-dnf module enable nginx:1.24 -y
+dnf module enable nginx:1.24 -y &>> $Log
 
 echo "Installing the nagix "
-dnf install nginx -y
-echo "Dowanloading the UI of $Component "
-curl -L -o /tmp/$Component.zip https://stan-robotshop.s3.amazonaws.com/$Component-v3.zip
+dnf install nginx -y &>> $Log
+echo "Dowanloading the UI of $Component " &>> $Log
+curl -L -o /tmp/$Component.zip https://stan-robotshop.s3.amazonaws.com/$Component-v3.zip &>> $Log
 
 echo "CleanUp Job"
-cd /usr/share/nginx/html
-rm -rf *
+cd /usr/share/nginx/html &>> $Log
+rm -rf * &>> $Log
 
 echo "Unziping the folder"
-unzip /tmp/$Component.zip
-echo "Starting the $Component servive"
-systemctl enable nginx
-systemctl restart nginx
+unzip /tmp/$Component.zip &>> $Log
+echo "Starting the $Component servive" 
+systemctl enable nginx &>> $Log
+systemctl restart nginx &>> $Log
