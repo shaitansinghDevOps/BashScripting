317dbb1a0be95ebd4b3622acd6162e6f030f3d2d
satyam singh <satyamsingh@satyams-MacBook-Air.local>
2026-05-07 23:50:02 +0530

    first commit

--- RoboShopProjectAutomation/frontend.sh
+++ RoboShopProjectAutomation/frontend.sh
@@ -1,52 +1,57 @@
 #!/bin/bash
 
 echo "Configuration management for frontend in progress"
+
 ID=$(id -u)
-Component="frontend"
-Log=$("/tmp/${Component}.log")
+COMPONENT="frontend"
+LOG="/tmp/${COMPONENT}.log"
+
 if [ $ID -ne 0 ]
 then
- echo "script has to excute as root user"
- echo -e "Example usage:\n\t \e[32msudo bash $0 OR bash $0\e[0m"
- exit 1
+   echo "Script has to execute as root user"
+   echo -e "Example usage:\n\t \e[32msudo bash $0 OR bash $0\e[0m"
+   exit 1
 fi
-stat()
-{
-    if [$1 -eq 0]; then
-     echo -e "\e[32m sucuess \e[0m"
-    else 
-      echo -e "\e[33m sucuess \e[0m"
+
+stat() {
+   if [ $1 -eq 0 ]
+   then
+      echo -e "\e[32m Success \e[0m"
+   else
+      echo -e "\e[31m Failure \e[0m"
       exit 2
-    fi
+   fi
 }
-echo "disbling the nagnix"
-dnf module disable nginx -y &>> $Log
+
+echo "Disabling nginx"
+dnf module disable nginx -y &>> $LOG
 stat $?
 
-echo "enbling nagix"
-dnf module enable nginx:1.24 -y &>> $Log
+echo "Enabling nginx"
+dnf module enable nginx:1.24 -y &>> $LOG
 stat $?
 
-echo "Installing the nagix "
-dnf install nginx -y &>> $Log
+echo "Installing nginx"
+dnf install nginx -y &>> $LOG
 stat $?
 
-echo "Dowanloading the UI of $Component " &>> $Log
-curl -L -o /tmp/$Component.zip https://stan-robotshop.s3.amazonaws.com/$Component-v3.zip &>> $Log
+echo "Downloading the UI of $COMPONENT"
+curl -L -o /tmp/$COMPONENT.zip https://stan-robotshop.s3.amazonaws.com/$COMPONENT-v3.zip &>> $LOG
 stat $?
 
-echo "CleanUp Job"
-cd /usr/share/nginx/html &>> $Log
-rm -rf * &>> $Log
+echo "Cleanup Job"
+cd /usr/share/nginx/html &>> $LOG
+rm -rf * &>> $LOG
 stat $?
 
-echo "Unziping the folder"
-unzip /tmp/$Component.zip &>> $Log
-stat $?
-echo "Enbling the $Component servive" 
-systemctl enable nginx &>> $Log
+echo "Unzipping the folder"
+unzip /tmp/$COMPONENT.zip &>> $LOG
 stat $?
 
-echo "Starting  the $Component servive" 
-systemctl restart nginx &>> $Log
+echo "Enabling the $COMPONENT service"
+systemctl enable nginx &>> $LOG
+stat $?
+
+echo "Starting the $COMPONENT service"
+systemctl restart nginx &>> $LOG
 stat $?
